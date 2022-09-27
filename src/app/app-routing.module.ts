81ecf5c930f4b5641ff54547474387ca012b56ac
LeandroCarlino <leandro.ca.98@gmail.com>
2022-09-27 13:23:37 -0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
 
 const routes: Routes = [
-  {path: 'porpais', component: PorPaisComponent},
-  {path: 'porcapital', component: PorCapitalComponent},
-  {path: 'porregion', component: PorRegionComponent}
+  {path: '', component: PorPaisComponent, pathMatch: 'full'},
+  {path: 'pais', component: PorPaisComponent},
+  {path: 'capital', component: PorCapitalComponent},
+  {path: 'region', component: PorRegionComponent},
+  {path: 'pais/:id', component: VerPaisComponent},
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
