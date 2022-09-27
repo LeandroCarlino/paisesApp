d6a87b8c093b1714162a864f565afae395000ee9
LeandroCarlino <leandro.ca.98@gmail.com>
2022-09-27 12:57:56 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'porpais', component: PorPaisComponent},
+  {path: 'porcapital', component: PorCapitalComponent},
+  {path: 'porregion', component: PorRegionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
